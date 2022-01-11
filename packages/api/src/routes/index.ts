593027810877abb52b16b822aa4ea304367a3b2f
Jpuf <38541170+Jpuf0@users.noreply.github.com>
2022-01-11 09:23:18 +0000

    added health check

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -12,6 +12,15 @@ router.get("/", (_, res) => {
   });
 })
 
+router.get("/health", (_, res) => {
+	res.api(200, {
+		message: "OK",
+		hostname: HOSTNAME,
+		env: process.env.NODE_ENV,
+		uptime: Math.floor(process.uptime() * 1000),
+	});
+});
+
 router.use((_, res) => {
   res.api(404, {
     message: "Not Found"
