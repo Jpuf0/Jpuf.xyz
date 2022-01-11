f18fdc8fa8a3f630940ef2e6c77b058b1561a6d9
Jpuf <38541170+Jpuf0@users.noreply.github.com>
2022-01-11 09:23:57 +0000

    remove redis password from redis connection

--- packages/api/src/db/redis/index.ts
+++ packages/api/src/db/redis/index.ts
@@ -1,11 +1,10 @@
 import Redis from "ioredis";
-import { API_HOSTNAME, REDIS_HOST, REDIS_PASS, REDIS_PORT } from "../../config";
+import { API_HOSTNAME, REDIS_HOST, REDIS_PORT } from "../../config";
 
 export const redis = new Redis({
 	keyPrefix: `${API_HOSTNAME}:`,
 	host: REDIS_HOST,
-	port: REDIS_PORT,
-	password: REDIS_PASS
+	port: REDIS_PORT
 });
 
 redis.on("error", (error) => {
